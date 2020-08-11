d3950cfbf5625eb393e073440e3b49f9d91656c2
EphafroditeMin <vhf201@naver.com>
2020-08-11 23:13:50 +0900

    Extract indeed data and organize them in a good way.
    Extract indeed data and organize them in a good way.

--- WebScapper2_main.py
+++ WebScapper2_main.py
@@ -1,6 +1,8 @@
-1from indeed import extract_indeed_pages, extract_indeed_jobs
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
 
 last_indeed_page = extract_indeed_pages()
 
 indeed_jobs = extract_indeed_jobs(last_indeed_page)
+
+print(indeed_jobs)
