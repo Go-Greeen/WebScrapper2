f6b8b2b86c07bdcdedf8a6eb012ebc699de20d17
EphafroditeMin <vhf201@naver.com>
2020-08-15 00:01:58 +0900

    Update WebScrapper_indeed.py

--- WebScrapper_indeed.py
+++ WebScrapper_indeed.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 LIMIT = 50
 URL = f"https://kr.indeed.com/jobs?q=python&limit={LIMIT}"
 
-def extract_indeed_pages():
+def get_last_page():
   result = requests.get(URL)
   soup = BeautifulSoup(result.text, "html.parser")
   pagination = soup.find("div", {"class":"pagination"})
@@ -18,7 +18,7 @@ def extract_indeed_pages():
 
 
 
-def extract_indeed_jobs(last_page):
+def extract_jobs(last_page):
   
   jobs = []
   for page in range(last_page):
@@ -36,8 +36,7 @@ def extract_indeed_jobs(last_page):
 def extract_job(html):
     title = html.find("h2", {"class":"title"}).find("a")["title"]
     location = html.find("div", {"class":"recJobLoc"})["data-rc-loc"]
-    job_id = html["data-jk"]
-    
+    job_id = html["data-jk"]    
     company = html.find("span", {"class":"company"})
     company_anchor = company.find("a")
     if company_anchor is not None:
@@ -46,3 +45,12 @@ def extract_job(html):
       company = str(company.string)
     company = company.strip()
     return{'title': title,'company': company, 'location': location, 'link': f"https://kr.indeed.com/jobs?q=python&limit=50&radius=25&start=100&vjk={job_id}"}
+
+
+
+
+def get_jobs():
+  last_page = get_last_page()
+  jobs = extract_jobs(last_page)
+  return jobs
+ 
